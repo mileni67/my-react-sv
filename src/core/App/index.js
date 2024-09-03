a59212ff6a4ef1be142c3d4f6fbd5878c6079b13
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-09-03 19:59:56 +0200

    added main index.js

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,5 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../common/themeSlice";
+import { themeDark, themeLight } from "./theme";
+import { ThemeSwitch } from "../../common/ThemeSwitch";
+
 export const App = () => {
-  return <h1>Hello,World!</h1>;
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <ThemeSwitch />
+      <Normalize />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
 
 export default App;
