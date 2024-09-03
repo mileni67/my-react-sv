de31ae70a4a4a3973fd628ccbf3e5993ee420369
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-09-03 19:59:26 +0200

    added theme for style

---
+++ src/core/App/theme.js
@@ -0,0 +1,42 @@
+export const colorNames = {
+  Moonlight: "#FBFBFE",
+  white: "#FFFFFF",
+  mercury: "#e5e5e5",
+  mineShaft: "#252525",
+  doveGray: "#6D6D6D",
+};
+
+const common = {
+  breakpoints: {
+    mobileMax: 767,
+    tableVertical: 991,
+    tableHorizontal: 1999,
+  },
+};
+
+export const themeLight = {
+  ...common,
+  colors: {
+    site: {
+      background: colorNames.Moonlight,
+    },
+    themeSwitch: {
+      background: colorNames.mercury,
+      icon: colorNames.white,
+    },
+  },
+};
+
+export const themeDark = {
+  ...common,
+  colors: {
+    site: {
+      background: colorNames.mineShaft,
+      text: colorNames.white,
+    },
+    themeSwitch: {
+      background: colorNames.doveGray,
+      icon: colorNames.mineShaft,
+    },
+  },
+};
