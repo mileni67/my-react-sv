af2117ba95599a462b3203228dfc6ce4dff3f160
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-09-04 20:16:51 +0200

    added skills

---
+++ src/features/personalHomePage/skillData.js
@@ -0,0 +1,34 @@
+export const skills = [
+    "Semantic & accesible HTML",
+    "Error handling",
+    "Git",
+    "Responsive Web Design",
+    "React",
+    //eslint-disable-next-line no-script-url
+    "JavaScript: ES6+",
+    "React Hooks",
+    "NPM",
+    "Working with API (fetch, axios)",
+    "Redux (Toolkit)",
+    "Immutability",
+    "Promises, Async/Await",
+    "Redux-Saga",
+    "MarkDown",
+    "Debugging",
+    "React Router",
+    "Scrum",
+    "CSS Flexbox",
+    "CSS Grid",
+    "Trello",
+    "TeamWork",
+    "CSS BEM convention",
+  ];
+  
+  export const nextSkills = [
+    "JS classes",
+    "Unit testing",
+    "Node.js",
+    "TypeScript",
+    "Cypress",
+    "React Context",
+  ];
