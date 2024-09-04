1e2cf46342e6b952c77ccd0050a9615864a4c8d5
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-09-04 20:16:36 +0200

    added new file  persoanmHomePage

---
+++ src/features/personalHomePage/index.js
@@ -0,0 +1,9 @@
+import { ThemeSwitch } from "../../common/ThemeSwitch";
+import { MainInforamation } from "./MainInformation";
+
+export const PersonalHomePage = () => (
+  <div>
+    <ThemeSwitch />
+    <MainInforamation />
+  </div>
+);
