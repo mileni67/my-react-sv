f930e24cbc64934c6c03ad288c25f539dda7549a
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-09-11 19:35:45 +0200

    added my Skills

---
+++ src/features/personalHomePage/Skills/index.js
@@ -0,0 +1,17 @@
+import { Bullet, Item, List, Section, StyledHeader } from "./styled";
+
+export const Skills = ({ title, skills }) => {
+  return (
+    <Section>
+      <StyledHeader>{title}</StyledHeader>
+      <List>
+        {skills.map((skill) => (
+          <Item key={skill}>
+            <Bullet />
+            {skill}
+          </Item>
+        ))}
+      </List>
+    </Section>
+  );
+};
